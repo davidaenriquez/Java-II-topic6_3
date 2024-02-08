5595e4a9180ca5b79faf60cf8c8d87f767bbd662
David Enriquez <david.a.enriquez.pro@gmail.com>
2024-02-08 12:36:20 -0700

    initial commit, created an asynchronous client-server application by using java thread, added javadocs and comments to all methods

--- src/app/ServerThread.java
+++ src/app/ServerThread.java
@@ -2,19 +2,28 @@ package app;
 
 import java.io.IOException;
 
+/**
+ * The ServerThread class represents a thread that starts an instance of the Server.
+ * It extends the Thread class and overrides the run() method to execute the server logic.
+ */
 public class ServerThread extends Thread {
-	// You need to override the run() to put the code that will run in this thread
-	public void run() {
-		// Create an instance of a Server
-		// Start the Server on port 6666 (which will not return until the Shutdown Command is received)
-		// and then on exit clean everything up which will exit this thread
-		Server server = new Server();
-		try {
-			server.start(6666);
-			server.cleanup();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+
+    /**
+     * Override the run() method to execute the server logic.
+     */
+    @Override
+    public void run() {
+        // Create an instance of a Server
+        Server server = new Server();
+
+        try {
+            // Start the Server on port 6666 (which will not return until the Shutdown Command is received)
+            server.start(6666);
+
+            // On exit, clean everything up which will exit this thread
+            server.cleanup();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
